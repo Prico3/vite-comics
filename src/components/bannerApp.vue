b70de6ad028699d199d896e85b344173f3d840fd
Prico3 <pricoco.andrea@gmail.com>
2022-11-09 16:46:12 +0100

    banner added

---
+++ src/components/bannerApp.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="banner">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.banner {
+    background-image: url("../assets/jumbotron.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    height: 300px;
+}
+</style>
